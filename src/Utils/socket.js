31b26c949958698a950e19692da810684c45dcf1
SOUMAYSIKCHI <120584256+SOUMAYSIKCHI@users.noreply.github.com>
2025-06-08 00:35:36 +0530

    Update socket.js

--- src/Utils/socket.js
+++ src/Utils/socket.js
@@ -2,7 +2,7 @@ const baseSocketURL =
   location.hostname === "localhost"
     ? "http://localhost:3300"
     : "https://devsync.co.in";
-
+let socket;
 export const createSocketConnection = () => {
   if (!socket) {
     socket = io(baseSocketURL, {
