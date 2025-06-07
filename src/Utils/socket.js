6e0879fd436f8359cf12a021f9d9de87f64f8bff
SOUMAYSIKCHI <120584256+SOUMAYSIKCHI@users.noreply.github.com>
2025-06-08 00:41:47 +0530

    Update socket.js

--- src/Utils/socket.js
+++ src/Utils/socket.js
@@ -1,17 +1,9 @@
 import { io } from "socket.io-client";
 
-const baseSocketURL =
-  location.hostname === "localhost"
-    ? "http://localhost:3300"
-    : "https://devsync.co.in";
-let socket;
 export const createSocketConnection = () => {
-  if (!socket) {
-    socket = io(baseSocketURL, {
+    return io("https://devsync.co.in", {
       path: "/api/socket.io",
       withCredentials: true,
       transports: ["websocket", "polling"],
     });
-  }
-  return socket;
 };
